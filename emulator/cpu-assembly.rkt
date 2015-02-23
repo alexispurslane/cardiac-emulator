3e2d555d294f3642ba8aa62e8b0dc59b4088ea08
christopherdumas <christopherdumas@me.com>
2015-02-22 18:11:01 -0800

    Pretty much finished exept for heigh-level assembly features and memory storage, which is whaky

--- emulator/cpu-assembly.rkt
+++ emulator/cpu-assembly.rkt
@@ -7,11 +7,8 @@
  example-code)
 
 (define example-code (string-join '(
-				    "inp 2"
-				    "cla 2"
-				    "inp 3"
-				    "add 3"
-				    "nop -1"
+				    "inp 7"
+				    "hrs -1"
 				    ) "\n"))
 
 (define-syntax match-lambda
@@ -37,27 +34,29 @@
 		 ["jmp" 8]
 		 ["hrs" 9]
 		 ["nop" -1]) `(,(string->number (second instruction))))) real-code))
-
-(define (cpu-execute code memory-map-start input-slot output-slot-start acc-start [pc (first memory-map-start)])
+(define (split-by lst n)
+  (if (not (empty? lst))
+      (cons (take lst n) (split-by (drop lst n) n))
+      '()))
+(define (cpu-execute memory-map-start input-slot output-slot-start acc-start)
+  (define begin-pc (memory-access memory-map-start 0))
+  (define memory-map-code-split (split-by memory-map-start 2))
+  (define code (append (take-right memory-map-code-split begin-pc)
+		       (dropf-right memory-map-code-split (lambda (e)
+							    (= (first e) 9)))))
   (foldl (lambda (instruction i prev-state)
+	   (define pc (+ begin-pc i))
 	   (define memory-map (or (first prev-state) memory-map-start))
 	   (define acc (or (second prev-state) acc-start))
 	   (define output-slot (or (third prev-state) output-slot-start))
-	   (displayln (memory-access memory-map 2))
-	   (displayln acc)
-	   (displayln "---")
 	   (match instruction
 	     [`(0 ,mloc) (list (change-memory-map memory-map `((,input-slot ,mloc))) acc output-slot)]
 	     [`(1 ,mloc)
 	      (list memory-map (memory-access memory-map mloc) output-slot)]
 	     [`(2 ,mloc)
-	      (display ":: ")
-	      (displayln memory-map)
-	      (display ":: ")
-	      (displayln (memory-access memory-map mloc))
 	      (list memory-map (+ acc (memory-access memory-map mloc)) output-slot)]
 	     [`(3 ,num) (if (negative? acc)
-			    (cpu-execute (remove (sub1 num) code) memory-map input-slot output-slot acc)
+			    (cpu-execute memory-map input-slot output-slot acc (+ pc num))
 			    (list memory-map acc output-slot))]
 	     [`(4 ,num)  (list memory-map (arithmetic-shift num acc) output-slot)]
 	     [`(5 ,mloc) (list memory-map acc (memory-access memory-map mloc))]
@@ -66,5 +65,9 @@
 	     [`(8 ,num) (cpu-execute (remove (sub1 num) code) memory-map input-slot output-slot acc i)]
 	     [else (list memory-map acc output-slot)])) '(#f #f #f) code (build-list (length code) values)))
 
-(define (create-cpu code input)
-  (cpu-execute (cpu-encode code) (create-memory-map) input 0 0))
+(define (create-cpu start-mem code input)
+  (define memory-map (change-memory-map (create-memory-map) (map (lambda (e i)
+								   `(,e ,(+ start-mem i)))
+								 (flatten (cpu-encode code))
+								 (build-list (length (flatten (cpu-encode code))) values))))
+  (cpu-execute memory-map input 0 0))
